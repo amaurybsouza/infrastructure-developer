a17e654434268c4adf9b3ab5a68e70a15cdc80ed
“amaurybsouza” <amaurybsouza>
2020-02-27 08:00:29 -0300

    teste

--- terraform-ec2-launch-instances/instances.tf
+++ terraform-ec2-launch-instances/instances.tf
@@ -1,6 +1,28 @@
+#Configure AWS provider
 provider "aws" {
-  region = "ea-east-1"
+  access_key = "asasasasas"
+  secret_key = "asasasasasas"
+  region = "us-east-1"
+  shared_credentials_file = "/home/absouza/.aws/credentials"
+  profile = "boblee"
 }
+
+resource "aws_instance" "example" {
+  ami = "dsdsdsdsdsd"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.my_key.key_name}"
+}
+
+
+
+
+
+
+
+
+
+
+
 
 resource "type" "name" {
   
